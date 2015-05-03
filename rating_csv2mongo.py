e9f4fa1ddfb01de7c83e43a77ce494797cbeb52c
BernhardKonrad <bernhard.konrad@gmail.com>
2015-05-02 21:43:07 -0700

    Adds new votes from up to end of April 2015.

--- rating_csv2mongo.py
+++ rating_csv2mongo.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     with open("rating_pid_url.csv", "r") as f:
         next(f)
         for line in f:
-            userID, pageID, rating_100, time, q_url = line.split(',')[1:]
+            userID, pageID, rating_100, time, q_url = line.split(',')
             rating_100 = int(rating_100)
             rating_5 = (rating_100 - 1) / 20 + 1  # 1-20 -> 1, 81-100 -> 5
             questionID = url2questionID(q_url)
@@ -57,6 +57,6 @@ if __name__ == '__main__':
                 votes.insert(vote)
             except DuplicateKeyError:
                 print("Duplicate entry found - skipping.")
-                break
+                # break
 
     print("Total number of votes: %d" % votes.count())
